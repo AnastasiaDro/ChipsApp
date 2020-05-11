4c1196c494fc69bf779a500a230b63ed31430a95
AnastasiaDro <adrogunova@gmail.com>
2020-05-11 21:52:55 +0300

    - started make custom View of chip

---
+++ app/src/main/java/com/geekbrains/chipsapp/ChipsFragment/ChipView.java
@@ -0,0 +1,15 @@
+package com.geekbrains.chipsapp.ChipsFragment;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.View;
+
+public class ChipView extends View {
+    private int baseColor = Color.GRAY;
+    private int checkedColor = Color.RED;
+
+
+    public ChipView(Context context) {
+        super(context);
+    }
+}
