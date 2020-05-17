b4ccc3d684dc6f3b79601505ac08405cd67f9bba
AnastasiaDro <adrogunova@gmail.com>
2020-05-17 11:59:44 +0300

    -cleared code again

--- app/src/main/java/com/geekbrains/chipsapp/ChipsNumAlert.java
+++ app/src/main/java/com/geekbrains/chipsapp/ChipsNumAlert.java
@@ -1,8 +1,5 @@
 package com.geekbrains.chipsapp;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
